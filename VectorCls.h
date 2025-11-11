923eb3a3977630d361cb9b6a3653f25d655f547e
Levent Bekdemir <lvntbkdmr@gmail.com>
2025-11-11 23:25:41 +0300

    some kind of updates

--- VectorCls.h
+++ VectorCls.h
@@ -3,6 +3,7 @@
 
 #include "CommonTypes.h"
 #include "SubMatPtrCls.h"
+#include "MatrixCls.h"
 
 template <UINT32 Length, class ElementType>
 class VectorCls
@@ -16,25 +17,32 @@ public:
     
     ElementType& operator()(UINT32 index);
     ElementType operator()(UINT32 index) const;
+
+    void setLength(UINT32 length);
     
-    UINT32 getLength() const { return Length; }
+    UINT32 getLength() const { return SubLength; }
+    
+    // Construct bidiagonal difference matrix
+    void BidiagonalDifference(MatrixCls<Length, Length, ElementType>& result) const;
+
+    UINT32 SubLength;
 
     ElementType Elements[Length];
 };
 
 template <UINT32 Length, class ElementType>
-VectorCls<Length, ElementType>::VectorCls()
+VectorCls<Length, ElementType>::VectorCls() : SubLength(Length)
 {
-    for(UINT32 i = 0; i < Length; i++)
+    for(UINT32 i = 0; i < SubLength; i++)
     {
         Elements[i] = ElementType(0);
     }
 }
 
 template <UINT32 Length, class ElementType>
-VectorCls<Length, ElementType>::VectorCls(const ElementType& value)
+VectorCls<Length, ElementType>::VectorCls(const ElementType& value) : SubLength(Length)
 {
-    for(UINT32 i = 0; i < Length; i++)
+    for(UINT32 i = 0; i < SubLength; i++)
     {
         Elements[i] = value;
     }
@@ -50,6 +58,14 @@ SubMatPtrCls<Length, 1, ElementType> VectorCls<Length, ElementType>::operator()(
 {
     SubMatPtrCls<Length, 1, ElementType> subVecPtr;
     
+    // Bounds check
+    if (start + length > SubLength || length > Length)
+    {
+        subVecPtr.SubRowNum = 0;
+        subVecPtr.SubColNum = 0;
+        return subVecPtr;
+    }
+    
     for(UINT32 i = 0; i < length; i++)
     {
         subVecPtr.ElementsPtr[i][0] = &(Elements[start + i]);
@@ -64,13 +80,62 @@ SubMatPtrCls<Length, 1, ElementType> VectorCls<Length, ElementType>::operator()(
 template <UINT32 Length, class ElementType>
 ElementType& VectorCls<Length, ElementType>::operator()(UINT32 index)
 {
+    // Bounds check: ensure index is within runtime size
+    if (index >= SubLength)
+    {
+        // Return first element as fallback (or could throw/assert)
+        return Elements[0];
+    }
     return Elements[index];
 }
 
 template <UINT32 Length, class ElementType>
 ElementType VectorCls<Length, ElementType>::operator()(UINT32 index) const
 {
+    // Bounds check: ensure index is within runtime size
+    if (index >= SubLength)
+    {
+        // Return zero as fallback (or could throw/assert)
+        return ElementType(0);
+    }
     return Elements[index];
+}
+
+template <UINT32 Length, class ElementType>
+void VectorCls<Length, ElementType>::setLength(UINT32 length)
+{
+    // Bounds check: ensure length doesn't exceed compile-time capacity
+    if (length > Length)
+    {
+        SubLength = Length; // Cap at maximum capacity
+    }
+    else
+    {
+        SubLength = length;
+    }
+}
+
+template <UINT32 Length, class ElementType>
+void VectorCls<Length, ElementType>::BidiagonalDifference(MatrixCls<Length, Length, ElementType>& result) const
+{
+    // Set matrix size: (SubLength - 1) rows x SubLength columns
+    // Main diagonal uses rows 0 to SubLength-2, superdiagonal uses columns 0 to SubLength-1
+    result.setSubMatrixSize(SubLength - 1, SubLength);
+    
+    // Initialize to zero
+    result.Zeros();
+    
+    // Fill main diagonal with vector elements (up to SubLength - 1)
+    for (UINT32 i = 0; i < SubLength - 1; i++)
+    {
+        result(i, i) = Elements[i];
+    }
+    
+    // Fill first superdiagonal with -1 * Elements[i+1] at position (i, i+1)
+    for (UINT32 i = 0; i < SubLength - 1; i++)
+    {
+        result(i, i + 1) = ElementType(-1) * Elements[i + 1];
+    }
 }
 
 #endif
